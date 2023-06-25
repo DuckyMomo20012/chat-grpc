f46e04f08412119291f86e7e2ea527842b9554e8
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-06-25 23:15:18 +0700

    feat(server): update chat service

--- src/server/internal/chat/services/chat.py
+++ src/server/internal/chat/services/chat.py
@@ -1,7 +1,60 @@
+import grpc
+from google.protobuf.timestamp_pb2 import Timestamp
+
 import pkg.protobuf.chat_service.chat_service_pb2 as chat_service_pb2
 import pkg.protobuf.chat_service.chat_service_pb2_grpc as chat_service_pb2_grpc
+import src.server.internal.chat.entities.message as message
 
 
 class ChatService(chat_service_pb2_grpc.ChatServiceServicer):
-    def HealthCheck(self, request, context):
+    async def Send(self, request, context):
+        userId = context.user_id
+
+        await message.Message.create(user_id=userId, content=request.content)
+
+        return chat_service_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
+
+    async def React(self, request, context):
+        userId = context.user_id
+
+        isUserReacted = await message.Reaction.filter(
+            user_id=userId, message_id=request.message_id
+        ).exists()
+
+        if isUserReacted:
+            context.set_details("User already reacted")
+            context.set_code(grpc.StatusCode.ALREADY_EXISTS)
+            return chat_service_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
+
+        await message.Reaction.create(user_id=userId, message_id=request.message_id)
+
+        return chat_service_pb2.google_dot_protobuf_dot_empty__pb2.Empty()
+
+    async def Fetch(self, request, context):
+        messages = await message.Message.all()
+
+        for msg in messages:
+            newCreatedTime = Timestamp()
+            newCreatedTime.FromDatetime(msg.created_time)
+
+            reactions = await msg.reactions.all()
+
+            newReactions = []
+            for reaction in reactions:
+                newReactions.append(
+                    chat_service_pb2.Reaction(
+                        user_id=f"{reaction.user_id}",
+                    )
+                )
+
+            yield chat_service_pb2.FetchResponse(
+                msg=chat_service_pb2.Message(
+                    message_id=f"{msg.id}",
+                    content=msg.content,
+                    created_time=newCreatedTime,
+                    reactions=newReactions,
+                )
+            )
+
+    async def HealthCheck(self, request, context):
         return chat_service_pb2.HealthCheckResponse(status="ok")
