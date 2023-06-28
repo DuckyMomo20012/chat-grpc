bda567506fededae9fecdd8dd68da93529e74430
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-06-28 17:54:54 +0700

    feat(server): add ignore endpoints for logging interceptor

--- src/shared/interceptor/logging_interceptor.py
+++ src/shared/interceptor/logging_interceptor.py
@@ -2,9 +2,14 @@ import logging
 
 import src.shared.interceptor.base_interceptor as base_interceptor
 
+ignoreEndpoints = ["/chat.v1.ChatService/Subscribe"]
+
 
 class LoggingInterceptor(base_interceptor.BaseInterceptor):
     async def intercept(self, method, request, context, method_name):
+        if method_name in ignoreEndpoints:
+            return await self.handleRequest(method, request, context)
+
         logging.info(f"{method_name}")
 
         return await self.handleRequest(method, request, context)
